5d46d7d828cd4ee236183302bb8166889ef1c3f7
Fluffpumpkin <61837182+Fluffpumpkin@users.noreply.github.com>
2024-06-24 11:41:51 -0700

    Webring and other things

--- webstring.js
+++ webstring.js
@@ -15,7 +15,12 @@ webring = {
     </div><br>
   `,
 
-  error: "<div>This site isn't part of the 3D Webring yet.</div>",
+  error: `
+	<div id="my-webring" style="gap:8px; border-style:solid; display:inline-block; border-color:#222133;line-height:0">
+	  <a href="https://old.fluffpumpkin.net/" target="_top"><img src="https://old.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
+	  <p>This site isn't part of the 3d webring yet</p>
+    </div><br>
+	`,
 
 };
 
@@ -25,9 +30,9 @@ webring = {
 webring.index = location.href.startsWith("file://") ? 0 : webring.sites.findIndex(url => location.href.startsWith("https://" + url));
 if (webring.index == -1) document.currentScript.outerHTML = webring.error;
 else {
-  webring.widget = webring.widget.replace("PREV", webring.sites.at(webring.index - 1));
-  webring.widget = webring.widget.replace("NEXT", webring.sites[(webring.index + 1) % webring.sites.length]);
-  webring.widget = webring.widget.replace("RANDOM", webring.sites[Math.floor(Math.random() * webring.sites.length)]);
+  webring.widget = webring.widget.replace("PREV", "https://" + webring.sites.at(webring.index - 1));
+  webring.widget = webring.widget.replace("NEXT", "https://" + webring.sites[(webring.index + 1) % webring.sites.length]);
+  webring.widget = webring.widget.replace("RANDOM", "https://" + webring.sites[Math.floor(Math.random() * webring.sites.length)]);
   document.currentScript.outerHTML = webring.widget;
 }
 delete webring;
