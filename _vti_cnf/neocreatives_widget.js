edea89b01e8ab00fc4b959257422b1e7ad4ad587
Fluffpumpkin <61837182+Fluffpumpkin@users.noreply.github.com>
2024-06-24 15:56:31 -0700

    Added cookies! (we love cookies)

--- _vti_cnf/neocreatives_widget.js
+++ _vti_cnf/neocreatives_widget.js
@@ -2,3 +2,6 @@ vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|24 Jun 2024 18:27:27 -0000
 vti_extenderversion:SR|6.0.2.8161
 vti_backlinkinfo:VX|home.html
+vti_author:SR|PHILLIS\\surfl
+vti_modifiedby:SR|PHILLIS\\surfl
+vti_timecreated:TR|24 Jun 2024 18:30:44 -0000
