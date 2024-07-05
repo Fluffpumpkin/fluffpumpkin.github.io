a732afde9ec3cc2353de7558f78d969266e4d3de
Fluffpumpkin <61837182+Fluffpumpkin@users.noreply.github.com>
2024-07-04 20:16:55 -0700

    Better cookies

--- _vti_cnf/webstring.js
+++ _vti_cnf/webstring.js
@@ -1,7 +1,7 @@
 vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|17 Jun 2024 05:20:22 -0000
 vti_extenderversion:SR|6.0.2.8161
-vti_backlinkinfo:VX|home.html
+vti_backlinkinfo:VX|landing.html
 vti_author:SR|PHILLIS\\surfl
 vti_modifiedby:SR|PHILLIS\\surfl
 vti_timecreated:TR|17 Jun 2024 09:02:18 -0000
