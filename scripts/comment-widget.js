45c27afcf471752bfe31772def638681bb51ab73
Breebee <61837182+Fluffpumpkin@users.noreply.github.com>
2025-01-04 15:29:17 -0700

    Changed link target in the comment widget

--- scripts/comment-widget.js
+++ scripts/comment-widget.js
@@ -382,6 +382,7 @@ function createComment(data) {
         site.innerText = s_websiteText;
         site.href = data.Website;
         site.className = 'c-site';
+        site.target = '_blank';
         comment.appendChild(site);
     }
 
