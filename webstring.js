902f9a2c3576bbdd26023f3c4f7a6970981d74e7
Fluffpumpkin <me@fluffpumpkin.net>
2025-08-05 15:08:31 -0700

    Changed all links over to the new structure

--- webstring.js
+++ webstring.js
@@ -4,20 +4,20 @@
 webring = {
 
   sites: [
-    "old.fluffpumpkin.net"
+    "www.fluffpumpkin.net"
   ],
 
   // PREV and NEXT get replaced with neighboring site urls
   widget: `
     <div id="my-webring" style="gap:8px; border-style:solid; display:inline-block; border-color:#222133;line-height:0">
-	  <a href="https://old.fluffpumpkin.net/" target="_top"><img src="https://old.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
-      <a href="PREV" target="_top"><img src="https://old.fluffpumpkin.net/images/prev.jpg" alt="<-prev "></a><a href="RANDOM" target="_top"><img src="https://old.fluffpumpkin.net/images/rand.jpg" alt="random"></a><a href="NEXT" target="_top"><img src="https://old.fluffpumpkin.net/images/next.jpg" alt=" next->"></a>
+	  <a href="https://www.fluffpumpkin.net/" target="_top"><img src="https://www.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
+      <a href="PREV" target="_top"><img src="https://www.fluffpumpkin.net/images/prev.jpg" alt="<-prev "></a><a href="RANDOM" target="_top"><img src="https://www.fluffpumpkin.net/images/rand.jpg" alt="random"></a><a href="NEXT" target="_top"><img src="https://www.fluffpumpkin.net/images/next.jpg" alt=" next->"></a>
     </div><br>
   `,
 
   error: `
 	<div id="my-webring" style="gap:8px; border-style:solid; display:inline-block; border-color:#222133;line-height:0">
-	  <a href="https://old.fluffpumpkin.net/" target="_top"><img src="https://old.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
+	  <a href="https://www.fluffpumpkin.net/" target="_top"><img src="https://www.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
 	  <p>This site isn't part of the 3d webring yet</p>
     </div><br>
 	`,
