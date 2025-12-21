4e7f9a59b8dfa4afc3111647909134cb646090bc
Fluffpumpkin <me@fluffpumpkin.net>
2025-12-21 12:44:42 -0600

    Update webstring.js

--- webstring.js
+++ webstring.js
@@ -11,14 +11,14 @@ webring = {
   // PREV and NEXT get replaced with neighboring site urls
   widget: `
     <div id="my-webring" style="gap:8px; border-style:solid; display:inline-block; border-color:#222133;line-height:0">
-	  <a href="https://www.fluffpumpkin.net/" target="_top"><img src="https://www.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
+	  <a href="https://www.fluffpumpkin.net/?p:webring" target="_top"><img src="https://www.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
       <a href="PREV" target="_top"><img src="https://www.fluffpumpkin.net/images/prev.jpg" alt="<-prev "></a><a href="RANDOM" target="_top"><img src="https://www.fluffpumpkin.net/images/rand.jpg" alt="random"></a><a href="NEXT" target="_top"><img src="https://www.fluffpumpkin.net/images/next.jpg" alt=" next->"></a>
     </div><br>
   `,
 
   error: `
 	<div id="my-webring" style="gap:8px; border-style:solid; display:inline-block; border-color:#222133;line-height:0">
-	  <a href="https://www.fluffpumpkin.net/" target="_top"><img src="https://www.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
+	  <a href="https://www.fluffpumpkin.net/?p:webring" target="_top"><img src="https://www.fluffpumpkin.net/images/3d_webstring.jpg" alt="3D Webring"></a><br>
 	  <p>This site isn't part of the 3d webring yet</p>
     </div><br>
 	`,
